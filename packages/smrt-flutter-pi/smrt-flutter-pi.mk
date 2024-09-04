080d6e8bd96b796ef9d79f6d835e6f200123b070
Marc Lainez <marc.lainez@gmail.com>
2024-09-04 15:12:53 +0200

    Initial commit

---
+++ packages/smrt-flutter-pi/smrt-flutter-pi.mk
@@ -0,0 +1,36 @@
+################################################################################
+#
+# smrt-flutter-pi
+#
+#################################################################################
+
+SMRT_FLUTTER_PI_VERSION = v0.3.1
+SMRT_FLUTTER_PI_SITE = $(call github,smartrent,flutter-pi,$(SMRT_FLUTTER_PI_VERSION))
+SMRT_FLUTTER_PI_LICENSE = MIT
+SMRT_FLUTTER_PI_LICENSE_FILES = LICENSE
+
+# THIS MUST MATCH THE ENGINE SHA1 OF GOOGLE_LIBFLUTTER
+SMRT_FLUTTER_PI_ENGINE_HASH=04817c99c9fd4956f27505204f7e344335810aed
+
+SMRT_FLUTTER_PI_DEPENDENCIES += google-libflutter libglvnd mesa3d libinput libxkbcommon xkeyboard-config freetype
+
+SMRT_FLUTTER_PI_CONF_OPTS = \
+	-DFLUTTER_ENGINE_SHA=$(SMRT_FLUTTER_PI_ENGINE_HASH) \
+
+# TODO: For some reason this is needed, investigation needed.
+define REINSTALL_LIBGLVND_LIBRARIES
+	cd $(BASE_DIR) ; make libglvnd-reinstall
+endef
+
+SMRT_FLUTTER_PI_PRE_BUILD_HOOKS += REINSTALL_LIBGLVND_LIBRARIES
+
+ifeq ($(BR2_PACKAGE_SMRT_FLUTTER_PI_ENABLE_DEBUG),y)
+	SMRT_FLUTTER_PI_CONF_OPTS += \
+		-DCMAKE_BUILD_TYPE=Debug \
+		-DCMAKE_C_FLAGS="-O1 -Wno-use-after-free"
+else
+	SMRT_FLUTTER_PI_CONF_OPTS += \
+		-DCMAKE_C_FLAGS="-Wno-use-after-free"
+endif
+
+$(eval $(cmake-package))
